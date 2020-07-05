a52ec294bd9b0e3b1ea6a47eb04d0f3f4598a0d7
dominicpasquali <dominicpasquali@users.noreply.github.com>
2020-07-05 14:26:16 -0700

    Update reference_number.py
    Remove file reference_number.py as this file is already present in the folder /share. Due to the instillation process, sharing file names without clear distinction will lead to unintended behavior.

--- scripts/reference_number.py
+++ scripts/reference_number.py
@@ -1,15 +1,1 @@
-import PixelNN.keras_utils as keras_utils
-
-def build_model(data_x, data_y):
-    return keras_utils.simple_model(
-        data_x,
-        data_y,
-        structure=[25, 20],
-        hidden_activation=keras_utils.Sigmoid2,
-        output_activation=keras_utils.Sigmoid2,
-        learning_rate=0.08,
-        weight_decay=1e-7,
-        momentum=0.4,
-        minibatch_size=60,
-        loss_function='categorical_crossentropy'
-    )
+#Remove file reference_number.py as this file is already present in the folder /share. Due to the instillation process, sharing file names without clear distinction will lead to unintended behavior.
